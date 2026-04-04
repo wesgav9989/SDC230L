53a090cba6d9cedd9759bd6def3cbbe48563a618
wesgav9989 <wesgav9989@students.ecpi.edu>
2026-04-03 21:55:20 -0400

    Calculator.java commit

---
+++ Week 5/Calculator.java
@@ -0,0 +1,29 @@
+/*******************************************************************
+* Name: Wesley Gavitt
+* Date: 4/3/2026
+* Assignment: Final Calculator Project
+*
+* This class performs basic mathematical operations.
+*******************************************************************/
+public class Calculator {
+
+    // Add two numbers
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    // Subtract two numbers
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    // Multiply two numbers
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    // Divide two numbers
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
